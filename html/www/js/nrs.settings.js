5d8c8f1a814aba12e35b9501845f6d4d782cfa35
Ghoom <r.goupil@free.fr>
2017-06-21 17:17:08 +0200

    Update nrs.settings.js

--- html/www/js/nrs.settings.js
+++ html/www/js/nrs.settings.js
@@ -50,34 +50,34 @@ var NRS = (function(NRS, $) {
 	};
 
 	NRS.languages = {
-		"de": "Deutsch",                 // german
+		//"de": "Deutsch",                 // german
 		"en": "English",                 // english
-		"es-es": "Español",              // spanish
-		"ca": "Català",                  // catalan
-		"fi": "Suomi (Experimental)",    // finnish
-		"fr": "Français",                // french
-		"gl": "Galego (Experimental)",   // galician
-		"el": "Ελληνικά (Experimental)", // greek
-		"sh": "Hrvatski (Experimental)", // croatian
-		"hi": "हिन्दी (Experimental)",  // hindi
-		"id": "Bahasa Indonesia",        // indonesian
-		"it": "Italiano",                // italian
-		"ja": "日本語",                   // japanese
-		"lt": "Lietuviškai",             // lithuanian
-		"nl": "Nederlands",              // dutch
-		"cs": "Čeština (Beta)",          // czech
-		"sk": "Slovensky (Beta)",        // slovakian
-		"pt-pt": "Português",            // portugese
-		"pt-br": "Português Brasileiro", // portugese, brazilian
-		"sr": "Српски (Experimental)",   // serbian, cyrillic
-		"sr-cs": "Srpski (Experimental)",// serbian, latin
-		"bg": "Български",               // bulgarian
-		"ro": "Român",                   // romanian
-		"tr": "Türk (Experimental)",     // turkish
-		"uk": "Yкраiнска",               // ukrainian
-		"ru": "Русский",                 // russian
-		"zh-cn": "中文 simplified",      // chinese simplified
-		"zh-tw": "中文 traditional"      // chinese traditional
+		//"es-es": "Español",              // spanish
+		//"ca": "Català",                  // catalan
+		//"fi": "Suomi (Experimental)",    // finnish
+		"fr": "Français"                 // french
+		//"gl": "Galego (Experimental)",   // galician
+		//"el": "Ελληνικά (Experimental)", // greek
+		//"sh": "Hrvatski (Experimental)", // croatian
+		//"hi": "हिन्दी (Experimental)",  // hindi
+		//"id": "Bahasa Indonesia",        // indonesian
+		//"it": "Italiano",                // italian
+		//"ja": "日本語",                   // japanese
+		//"lt": "Lietuviškai",             // lithuanian
+		//"nl": "Nederlands",              // dutch
+		//"cs": "Čeština (Beta)",          // czech
+		//"sk": "Slovensky (Beta)",        // slovakian
+		//"pt-pt": "Português",            // portugese
+		//"pt-br": "Português Brasileiro", // portugese, brazilian
+		//"sr": "Српски (Experimental)",   // serbian, cyrillic
+		//"sr-cs": "Srpski (Experimental)",// serbian, latin
+		//"bg": "Български",               // bulgarian
+		//"ro": "Român",                   // romanian
+		//"tr": "Türk (Experimental)",     // turkish
+		//"uk": "Yкраiнска",               // ukrainian
+		//"ru": "Русский",                 // russian
+		//"zh-cn": "中文 simplified",      // chinese simplified
+		//"zh-tw": "中文 traditional"      // chinese traditional
 	};
 
 	var userStyles = {};
