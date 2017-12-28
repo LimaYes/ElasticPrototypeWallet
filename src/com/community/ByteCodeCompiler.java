554c45662e1e6022be28c3ac23213f39fb21bceb
CryptoKittie <cryptokittie@blackhole>
2017-12-29 02:11:26 +1000

    better working postorder traversal

--- src/com/community/ByteCodeCompiler.java
+++ src/com/community/ByteCodeCompiler.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class ByteCodeCompiler {
+
     public static void build_bytecode(Primitives.STATE state) throws Exceptions.SyntaxErrorException {
         for (int i = state.ast_func_idx; i < state.stack_exp.size(); i++) {
             convert_function(state, state.stack_exp.get(i));
@@ -15,39 +16,44 @@ public class ByteCodeCompiler {
         return new ArrayList<Byte>();
     }
 
-    public static ArrayList<Byte> DFS(Primitives.AST root) {
+    static ArrayList<Byte> DFS(Primitives.AST root)
+    {
+
+        // Bytecode array
         ArrayList<Byte> byteCode = new ArrayList<>();
-        if( root == null ) return byteCode;
 
-        Stack<Primitives.AST> s = new Stack<Primitives.AST>();
-        Primitives.AST current = root;
+        // Create two stacks
+        Stack<Primitives.AST> s1 = new Stack<>();
+        Stack<Primitives.AST> s2 = new Stack<>();
 
-        s.add(root);
+        if (root == null)
+            return byteCode;
 
-        while( true ) {
+        // push root to first stack
+        s1.push(root);
 
-            if( current != null ) {
-                if( current.right != null )
-                    s.push( current.right );
-                s.push( current );
-                current = current.left;
-                continue;
-            }
+        // Run while first stack is not empty
+        while (!s1.isEmpty())
+        {
+            // Pop an item from s1 and push it to s2
+            Primitives.AST temp = s1.pop();
+            s2.push(temp);
 
-            if( s.isEmpty( ) )
-                return byteCode;
-            current = s.pop( );
-
-            if( current.right != null && ! s.isEmpty( ) && current.right == s.peek( ) ) {
-                s.pop( );
-                s.push( current );
-                current = current.right;
-            } else {
-                System.out.print( current.type + " " );
-                byteCode.addAll(compile(current));
-                current = null;
-            }
+            // Push left and right children of
+            // removed item to s1
+            if (temp.left != null)
+                s1.push(temp.left);
+            if (temp.right != null)
+                s1.push(temp.right);
         }
+
+        // Print all elements of second stack
+        while (!s2.isEmpty()) {
+            Primitives.AST temp = s2.pop();
+            System.out.print(temp.type + " (" + temp.svalue + ")\n");
+            byteCode.addAll(compile(temp));
+        }
+        return byteCode;
     }
 
     public static void convert_function(Primitives.STATE state, Primitives.AST root) throws Exceptions.SyntaxErrorException {
@@ -58,7 +64,7 @@ public class ByteCodeCompiler {
 
         ast_ptr = root;
 
-        System.out.println("PARSING " + root.svalue + "\n");
+        System.out.println("\nPARSING " + root.svalue);
         System.out.println("------------------------------------------");
 
         DFS(ast_ptr);
